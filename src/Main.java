b7c8818982bf28a2c310358d368ba4fdb88f6816
golden-hamster <isack19999@gmail.com>
2023-11-29 23:35:08 +0900

    #1 - 깃 공부 시작하기
    깃 공부를 합니다

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("2023-11-29 git study1");
+        System.out.println("브랜치를 만들었습니다");
     }
 }

2ce834a329f3ba36a702c932481b9023ccb620a3
golden-hamster <isack19999@gmail.com>
2023-11-30 17:31:58 +0900

    #5 - 노트북에서 개발합니다

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("2023-11-29 git study1");
         System.out.println("브랜치를 만들었습니다");
         System.out.println("데스크톱에서 개발합니다");
+        System.out.println("노트북에서 개발합니다.");
     }
 }
